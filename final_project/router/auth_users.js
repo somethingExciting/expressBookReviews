953602463a50fe7303def6eaeff4edaba2247d6a
Melissa Chow <Melissa.Chow@cdph.ca.gov>
2023-05-23 16:49:19 -0700

    adding authentication

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -16,7 +16,21 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const user = req.body.user;
+
+  if(!user) {
+    return res.status(404).json({message: "Body empty"});
+  }
+  
+  let accessToken = jwt.sign({
+    data: user
+  }, 'access', {expiresIn: 60 * 60});
+
+  req.session.authenticated = {
+    accessToken
+  }
+
+  return res.status(200).send("User successfully logged in");
 });
 
 // Add a book review
