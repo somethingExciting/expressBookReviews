b5faaa896b60b3d0c4bbb4f3517b53949de7708d
Melissa Chow <Melissa.Chow@cdph.ca.gov>
2023-05-24 14:47:31 -0700

    added get by isbn, author, & title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,19 +19,34 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(books[req.params.isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let bookVals = Object.values(books);
+  let allBooks = [];
+
+  for (let i = 0; i < Object.keys(books).length-1; i++) {
+    if (bookVals[i].author === req.params.author) {
+      allBooks.push(bookVals[i]);
+    }
+  }
+
+  return res.status(200).send(allBooks);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let bookVals = Object.values(books);
+
+  for (let i = 0; i < Object.keys(books).length-1; i++) {
+    if(bookVals[i].title === req.params.title) {
+      return res.status(200).send(bookVals[i]);
+    }
+  }  
 });
 
 //  Get book review
